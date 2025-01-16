1178ac05fa16c8fb7b200c416148ae507f002f25
Supreet singh <Supreetsingh1399@gmail.com>
2025-01-16 17:26:55 +0530

    feat: update server port configuration and add startup logging

--- Gym_App/Backend/MongoDB.ts
+++ Gym_App/Backend/MongoDB.ts
@@ -5,12 +5,11 @@ import cors from "cors";
 import path from "path";
 import Trainer from "../Models/Trainer_model";
 import User from "../Models/User_models";
-import axios from "axios";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const app = express();
-
+const Port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -32,6 +31,7 @@ const connectDB = async () => {
 };
 
 connectDB();
+console.log(`Server is running on port ${Port}`);
 
 // Post Routes to database
 app.post("/Register/Trainers", async (req: Request, res: Response) => {
